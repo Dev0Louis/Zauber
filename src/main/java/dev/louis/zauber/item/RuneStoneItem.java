7ab88436ec33b69f858b72294bec116a272b57e8
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-06-02 23:11:42 +0200

    Trinkets O.O

---
+++ src/main/java/dev/louis/zauber/item/RuneStoneItem.java
@@ -0,0 +1,9 @@
+package dev.louis.zauber.item;
+
+import dev.emi.trinkets.api.TrinketItem;
+
+public class RuneStoneItem extends TrinketItem {
+    public RuneStoneItem(Settings settings) {
+        super(settings);
+    }
+}
