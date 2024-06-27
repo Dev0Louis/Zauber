f93950ad7bcb4365541a835ab32d7cf7e41b2b03
Dev0Louis <Dev0Louis>
2024-06-27 18:44:37 +0200

    Improve PlayerFollowingEntity Movment. Use Actual Nebula not local one.

--- src/main/java/dev/louis/zauber/entity/PlayerFollowingEntity.java
+++ src/main/java/dev/louis/zauber/entity/PlayerFollowingEntity.java
@@ -1,21 +1,23 @@
 package dev.louis.zauber.entity;
 
 import eu.pb4.polymer.core.api.entity.PolymerEntity;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class PlayerFollowingEntity extends Entity implements PolymerEntity {
-    /*public static final EntityType<PlayerFollowingEntity> TYPE = FabricEntityTypeBuilder
+    public static final EntityType<PlayerFollowingEntity> TYPE = FabricEntityTypeBuilder
             .<PlayerFollowingEntity>create(SpawnGroup.MISC, PlayerFollowingEntity::new)
-            .build();*/
+            .build();
     private static final double HARD_TELEPORT_SQUARED_DISTANCE = Math.pow(12, 2);
-    private static final double NO_FOLLOW_SQUARED_DISTANCE = Math.pow(2.5, 2);
-    private static final double CLOSEST_SQUARED_DISTANCE = Math.pow(2, 2);
+    private static final double STAND_STILL_SQUARED_DISTANCE = Math.pow(2.5, 2);
+    private static final double MOVE_TO_PLAYER_SQUARED_DISTANCE = Math.pow(3, 2);
 
     private final PlayerEntity player;
 
@@ -32,21 +34,32 @@ public class PlayerFollowingEntity extends Entity implements PolymerEntity {
     public void tick() {
         super.tick();
         var target = player.getEyePos();
-        var vec = target.relativize(this.getPos());
-        var sqrtDistance = target.squaredDistanceTo(this.getPos());
+        double x = Math.sin(this.age / 15f) * 1.5;
+        double z = Math.cos(this.age / 15f) * 1.5;
+        target = target.add(x, 0, z);
+        var vec = this.getPos().relativize(target);
+        var sqrtDistance = target.squaredDistanceTo(this.getPos());;
 
-        Vec3d endPos;
         if (sqrtDistance > HARD_TELEPORT_SQUARED_DISTANCE) {
-            endPos = target.add(vec.normalize().multiply(2));
-        } else if (sqrtDistance < CLOSEST_SQUARED_DISTANCE) {
-            endPos = target.add(vec.multiply(1.1f));
-        } else if (sqrtDistance < NO_FOLLOW_SQUARED_DISTANCE){
-            endPos = this.getPos();
-        }else {
-            endPos = target.add(vec.multiply(0.9f));
+            this.setPosition(target);
+            return;
         }
 
-        this.setPosition(endPos);
+        if (sqrtDistance < STAND_STILL_SQUARED_DISTANCE && false) {
+            this.setVelocity(this.getVelocity().multiply(0.9));
+        } else {
+            this.addVelocity(vec.normalize().multiply(0.1));
+        }
+
+        this.velocityDirty = true;
+        this.move(MovementType.SELF, this.getVelocity());
+        //Debugger.addEntityBoundBox(this, this.getBoundingBox(), Color.BLACK);
+    }
+
+
+    @Override
+    public boolean collidesWith(Entity other) {
+        return false;
     }
 
     @Override
@@ -71,6 +84,6 @@ public class PlayerFollowingEntity extends Entity implements PolymerEntity {
 
     @Override
     public EntityType<?> getPolymerEntityType(ServerPlayerEntity player) {
-        return EntityType.HORSE;
+        return EntityType.SHULKER_BULLET;
     }
 }
