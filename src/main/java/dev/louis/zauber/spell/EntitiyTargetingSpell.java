7b3d8c7d2ef60a8795b84a4b60d377f89153d737
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-06-29 10:57:35 +0200

    Fix Entity Targeting Spell

--- src/main/java/dev/louis/zauber/spell/EntitiyTargetingSpell.java
+++ src/main/java/dev/louis/zauber/spell/EntitiyTargetingSpell.java
@@ -5,16 +5,23 @@ import dev.louis.nebula.api.spell.SpellType;
 import dev.louis.zauber.config.ConfigManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public abstract class EntitiyTargetingSpell extends Spell {
     private Entity entity;
 
     public EntitiyTargetingSpell(SpellType<?> spellType, PlayerEntity caster) {
         super(spellType, caster);
-        if (caster.raycast(ConfigManager.getServerConfig().entityTargetingDistance(), 0, false) instanceof EntityHitResult entityHitResult) entity = entityHitResult.getEntity();
-    }
 
+        var optionalEntity = getTargetedEntity(caster,);
+        optionalEntity.ifPresent(entity -> this.entity = entity);
+    }
 
     @Override
     public void cast() {
@@ -29,5 +36,22 @@ public abstract class EntitiyTargetingSpell extends Spell {
     @Override
     public boolean isCastable() {
         return castedOn() != null && super.isCastable();
+    }
+
+    public static Optional<Entity> getTargetedEntity(Entity entity) {
+        int maxDistance = ConfigManager.getServerConfig().entityTargetingDistance();
+        Vec3d startPos = entity.getEyePos();
+        Vec3d rotation = entity.getRotationVec(1.0F).multiply(maxDistance);
+        Vec3d endPos = startPos.add(rotation);
+        Box box = entity.getBoundingBox().stretch(rotation).expand(1.0);
+        int squaredMaxDistance = maxDistance * maxDistance;
+        Predicate<Entity> predicate = entityx -> !entityx.isSpectator() && entityx.canHit();
+
+        EntityHitResult entityHitResult = ProjectileUtil.raycast(entity, startPos, endPos, box, predicate, squaredMaxDistance);
+        if (entityHitResult == null) {
+            return Optional.empty();
+        } else {
+            return startPos.squaredDistanceTo(entityHitResult.getPos()) > (double) squaredMaxDistance ? Optional.empty() : Optional.of(entityHitResult.getEntity());
+        }
     }
 }
